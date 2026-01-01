97b396b9ff092ea1dc08a759da920694299346d7
Kushalv06 <kushalv1908@gmail.com>
2026-01-01 08:23:48 +0530

    added ph no

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,33 @@
 function saveContact() {
-    const name = document.getElementById("cname").value;
-    const email = document.getElementById("cemail").value;
-    const message = document.getElementById("cmessage").value;
+    const name = document.getElementById("cname").value.trim();
+    const email = document.getElementById("cemail").value.trim();
+    const phone = document.getElementById("cphone").value.trim();
+    const message = document.getElementById("cmessage").value.trim();
+
+    const phonePattern = /^[6789][0-9]{9}$/;
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.com$/;
+
+    if (!phonePattern.test(phone)) {
+        alert("Phone number must be 10 digits and start with 6, 7, 8, or 9");
+        return;
+    }
+
+    if (!emailPattern.test(email)) {
+        alert("Email must be valid and end with .com");
+        return;
+    }
+
+    if (name.length < 3) {
+        alert("Name must be at least 3 characters long");
+        return;
+    }
 
     const data =
-`Name: ${name}
-Email: ${email}
-Message: ${message}`;
+    `Name: ${name}
+    Email: ${email}
+    Phone: ${phone}
+    Message: ${message}`;
 
     const blob = new Blob([data], { type: "text/plain" });
     const link = document.createElement("a");
@@ -14,6 +35,7 @@ Message: ${message}`;
     link.download = "contact.txt";
     link.click();
 }
+
 
 function loadContact(event) {
     const file = event.target.files[0];
